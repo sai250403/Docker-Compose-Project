19b88da63956513d528c94565aa361a478763892
sai250403 <yvsganesh2003@gmail.com>
2025-01-16 12:44:58 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the Front-End!</h1>
+      <p>This is a basic React app connected to the back-end.</p>
+    </div>
+  );
+};
+
+export default App;
